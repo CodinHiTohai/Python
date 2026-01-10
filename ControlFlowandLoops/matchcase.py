e3516e0f51944a67009e7c4719dc43a84eab220b
GovindKumar <govindkumar52936@gmail.com>
2026-01-10 12:13:20 +0530

    matchcase

---
+++ ControlFlowandLoops/matchcase.py
@@ -0,0 +1,18 @@
+a=input("enter the starting letter of day so you go more detail")
+
+match(a):
+    case ('m'):
+        print("the day is Monday")
+    case ('t'):
+        print("the day is Tuesday")
+    case ('w'):
+        print("the day is wednesday")
+    case ('t'):
+        print("the day is Thursay")
+    case ('f'):
+        print("the day is Friday")
+    case ('s'):
+        print("the day is Saturday")
+    case _:
+        print("enter the the valuy day statin name")
+
