4d925b30090a1a93da3ea42e11fcabf82f1eaa69
GovindKumar <govindkumar52936@gmail.com>
2026-01-10 12:04:41 +0530

    ifelif

---
+++ ControlFlowandLoops/ifelif.py
@@ -0,0 +1,19 @@
+a=int(input("enter you age"))
+
+if(a>18):
+    print("you can drive")
+    print("thankyou for your interest")
+
+else:
+    print("you can't be apple to drive the car")
+
+
+
+age=int(input("enter your age"))
+
+if(age>18):
+    print("you can drive")
+elif(age==18):
+    print("thankyou for your interest wait some month please")
+else:
+    print("you can't be able to anythin")
