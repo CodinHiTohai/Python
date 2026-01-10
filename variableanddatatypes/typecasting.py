efd25b07c5fef1dcae90c383d22746a12ac134b0
GovindKumar <govindkumar52936@gmail.com>
2026-01-10 11:22:21 +0530

    typecasting

---
+++ variableanddatatypes/typecasting.py
@@ -0,0 +1,20 @@
+a=34 #this is integer
+b="5" #this is string
+d=4
+print(type(a))
+print(type(b))
+
+#convert the type b to int
+c=int(b)
+print(type(c))
+
+
+#d shoulb be typcase to string
+e=str(d)
+
+
+
+# int(): Converts to integer.
+# float(): Converts to float.
+# str(): Converts to string.
+# bool(): Converts to boolean.
