bdea158c38f4fe6d35ce1a58b8b8b1fc9ec2f15e
GovindKumar <govindkumar52936@gmail.com>
2026-01-10 11:03:21 +0530

    variable

---
+++ variableanddatatypes/variable.py
@@ -0,0 +1,13 @@
+age=34 #age is variabel so variable is not a complext think it is like a container or bottle;
+#in out home we have bottle to store oil and many thig like rice wheate and more thing we store in a bottle 
+#like in python we hava container,bottle ,or valriable to store data;
+ 
+name="govind" # String
+
+cgpa=4.4  #float
+
+# rules for defininig variable
+#variable are must start with(A-Z or a-z and "-")
+#varaible con conatian letter number and underscore
+# variable name are case sensitive(Govind and govind are different)
+#avoid using keywork like if elif and more keyword available in python
