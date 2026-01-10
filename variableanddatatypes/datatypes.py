e4d75c5b73ac42a07c6f8573a2bc60ddeeb9764f
GovindKumar <govindkumar52936@gmail.com>
2026-01-10 11:08:48 +0530

    datatypesinpython

---
+++ variableanddatatypes/datatypes.py
@@ -0,0 +1,17 @@
+# Integers (int): Whole numbers (e.g., 10, -5).
+# Floats (float): Decimal numbers (e.g., 3.14, -0.001).
+# Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
+# Booleans (bool): Represents True or False.
+# Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+# Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+# Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
+# Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+age =34 #int
+print(type(age))
+name="govind" # string
+print(type(name))
+cgpa=4.4; #float
+print(type(cgpa))
+
+valid=True  #boolean
+print(type(valid))
