03f81b3a59ef36747f4e3f7a4ecfa50a46314d99
GovindKumar <govindkumar52936@gmail.com>
2026-01-10 12:37:24 +0530

    breakcotinuepassstatement

---
+++ ControlFlowandLoops/breakcontinueandpass.py
@@ -0,0 +1,20 @@
+#break
+for i in range(1,21):
+     print(i)
+     if(i==13):
+        break
+
+#continue
+
+for i in range(1,22):
+    
+    if(i==7):
+        continue
+    print(i)
+
+
+#pass
+i=3;
+if(i==33):
+    pass
+print("end of proagrmme")
