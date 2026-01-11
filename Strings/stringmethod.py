857b091a4ac5ea4c63be27d1ccf150b84534af1e
GovindKumar <govindkumar52936@gmail.com>
2026-01-11 10:03:10 +0530

    stringmethod

---
+++ Strings/stringmethod.py
@@ -0,0 +1,16 @@
+name="govind kumar"
+print(name.upper())#this print all to character to capital
+name1="GOVIND KUMAR"
+print(name1.lower()) #this print alll the charater to lowercase
+
+name2=" govind kumar "
+print(name2.strip()) # this mean remove all the whitspaces from left and wright
+
+name3=" govind kumar "
+print(name3.rstrip()) #remove white spaces from the wright
+print(name3.lstrip()) #remove white spaces from the left
+
+
+print(name.replace("kumar","singh")) #place singh in place of kumar
+
+print(name.split()) # convert stirng to the list
