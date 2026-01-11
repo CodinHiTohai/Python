7f735763f8c318f8dde49160ea9d318d7b3aca04
GovindKumar <govindkumar52936@gmail.com>
2026-01-11 10:31:31 +0530

    question5

---
+++ Strings/question5.py
@@ -0,0 +1,12 @@
+# Take the string "  i love python programming  " and:
+
+# Remove extra spaces from both ends
+# Convert it to title case
+# Count how many times "o" appears
+# Check if the string "123abc" is alphanumeric.
+
+text=" i love python programming "
+print(text.strip())
+print(text.title())
+print(text.count('o'))
+print(text.isalnum())
