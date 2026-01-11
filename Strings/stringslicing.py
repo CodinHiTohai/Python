5245a20e633dadd7c077cf403d61b64b29572c2e
GovindKumar <govindkumar52936@gmail.com>
2026-01-11 09:53:29 +0530

    stringslicing

---
+++ Strings/stringslicing.py
@@ -0,0 +1,9 @@
+name="govind kumar"
+print(name[0:5]) #this count index 0 to 4:[g[0] o[1] v[2] [i][3] n[4]]
+
+print((name[:5])) #this also count 0 to 4 bacuse : python takes as 0
+
+print(name[4:]) # this means takes character form index 4
+
+
+print(name[::2])#this means takes index 0 the skip ex i take 2 so skip 2-1 index then take i
