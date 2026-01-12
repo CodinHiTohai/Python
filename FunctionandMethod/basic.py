4d7669d5c83c71d797d14cb7a26c217e58eac8e7
GovindKumar <govindkumar52936@gmail.com>
2026-01-12 10:41:28 +0530

    functionsbasics

---
+++ FunctionandMethod/basic.py
@@ -0,0 +1,27 @@
+# a=2
+# b=3
+# c=4
+
+# average=(a+b+c)/3;
+# print(average)
+
+
+# a1=5
+# b1=8
+# c1=4
+
+# average=(a1+b1+c1)/3;
+# print(average)
+
+# #if we need to change anythin you shoulb be go every poin and you need to be change
+# # so funtion is define as you go to change 1 place and you every 
+
+def average(a,b,c):
+    d= (a+b+c)/3
+    return d
+
+o1=average(4,55,3)#we use the funcion multiple times
+
+o2=average(55,44,33)
+print(o1)
+print(o2)
